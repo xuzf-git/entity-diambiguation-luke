2b7d54fd92d67e330995b60bd8d1605bf661cb29
linghan <ryou0634@gmail.com>
2021-02-09 01:43:06 +0900

    remove Multitask Iterator

--- luke/pretraining/batch_generator.py
+++ luke/pretraining/batch_generator.py
@@ -367,22 +367,3 @@ class DatasetSampler:
         data_size_list = [size ** smoothing_factor for size in data_size_list]
         size_sum = sum(data_size_list)
         return [size / size_sum for size in data_size_list]
-
-
-class MultitaskIterator:
-    def __init__(self, iterators: Dict[str, Iterator], gradient_accumulation_steps: int = 1):
-        self._iterators = iterators
-        self._gradient_accumulation_steps = gradient_accumulation_steps
-
-    def __iter__(self):
-        remaining = set(self._iterators.keys())
-
-        while remaining:
-            for task, batch_iterator in self._iterators.items():
-                if task in remaining:
-                    try:
-                        for _ in range(self._gradient_accumulation_steps):
-                            batch = next(batch_iterator)
-                            yield task, batch
-                    except StopIteration:
-                        remaining.remove(task)
